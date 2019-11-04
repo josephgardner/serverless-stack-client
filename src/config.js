1b7137b2089e997253c30baa0e967d4a26026b6c
Joseph Gardner <josephgardner@outlook.com>
2019-11-03 19:17:45 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "algoplay-notes-app-upload"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://ov6w8208vb.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_jsbYN0Oth",
+        APP_CLIENT_ID: "4v8fsehg5oh2b7iii0koigkubl",
+        IDENTITY_POOL_ID: "us-east-1:fd23bedc-8142-4ebd-b658-ae51d7b6b1d5"
+    }
+};
